a39a5655edbd4159ddd52d8959d500e3623ba014
diegosportcoachleiden <diegosportcoach@hotmail.com>
2026-09-09 15:47:55 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-ja const SUPABASE_URL = 'https://zjvqbfmxaibjcdpttgmj.supabase.co';
+const SUPABASE_URL = 'https://zjvqbfmxaibjcdpttgmj.supabase.co';
 const SUPABASE_KEY = 'sb_publishable_oZIVgG4DUG8zo6C1hoPkJA_x4YbnKkA';
 
 const supabaseClient = window.supabase.createClient(
