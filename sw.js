6d8fd3f8f0e781cb84462d683882e785c599ea14
diegosportcoachleiden <diegosportcoach@hotmail.com>
2026-09-10 22:02:15 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -17,8 +17,36 @@ self.addEventListener('activate', event => {
 
   self.clients.claim();
 });
+self.addEventListener('push', event => {
+  let data = {};
 
-self.addEventListener('fetch', event => {
+  if (event.data) {
+    data = event.data.json();
+  }
+
+  const title = data.title || 'DiegoSportCoach';
+
+  const options = {
+    body: data.body || 'Er is een nieuwe melding.',
+    data: {
+      url: data.url || '/diegosportcoach-app/'
+    }
+  };
+
+  event.waitUntil(
+    self.registration.showNotification(title, options)
+  );
+});
+
+self.addEventListener('notificationclick', event => {
+  event.notification.close();
+
+  event.waitUntil(
+    clients.openWindow(
+      event.notification.data.url || '/diegosportcoach-app/'
+    )
+  );
+});self.addEventListener('fetch', event => {
   event.respondWith(
     fetch(event.request, { cache: 'no-store' })
       .catch(() => caches.match(event.request))
